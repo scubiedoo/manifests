1373ac1a14ac66cb1eedba496cc31d7dc1e44470
Your Name <you@example.com>
2014-01-07 19:09:10 +0100

    linux build

--- Vagrantfile
+++ Vagrantfile
@@ -3,8 +3,8 @@ Vagrant.configure("2") do |config|
   config.vm.provider "virtualbox" do |v|
     v.name = "cubievm"
   end
-  config.apt_proxy.http  = "squid:80"
-  config.apt_proxy.https = "squid:80"
+#  config.apt_proxy.http  = "squid:80"
+#  config.apt_proxy.https = "squid:80"
   
   config.vm.provision :shell, path: "setupCubieVM.sh"
 end
--- createCubieVM.sh
+++ createCubieVM.sh
@@ -1,3 +1,28 @@
-#!/bin/bash
 
-source createCubieVM.bat
+run_ok()
+{
+	local CMD
+	CMD="$@"
+	local RET
+	echo "executing $CMD" 1>&2
+	eval $CMD
+	RET=$?
+	if [ $RET != 0 ]; then
+		echo "failed with code $RET" 1>&2
+		exit 1
+	fi
+}
+
+run_ok [ -r precise32.box ] || wget http://files.vagrantup.com/precise32.box
+
+run_ok vagrant box add precise32 precise32.box
+
+run_ok vagrant plugin install vagrant-proxyconf
+
+run_ok vagrant up
+
+#run_ok vagrant package --base cubievm --output cubievm.box --vagrantfile Vagrantfile.cubievm
+run_ok vagrant box add --force cubievm precise32.box
+run_ok vagrant box repackage cubievm virtualbox
+run_ok mv package.box cubievm.box
+
