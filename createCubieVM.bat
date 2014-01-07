c1771b51e403229984ff2fee54067311a3512072
scubiedoo <scubiedoo@users.noreply.github.com>
2014-01-07 17:14:23 +0100

    basic box creation

--- createCubieVM.bat
+++ createCubieVM.bat
@@ -1,7 +1,16 @@
+set HTTP_PROXY=squid:80/
+set HTTPS_PROXY=squid:80/
 
-mkdir cubievm
-cd cubievm
+cmd /c vagrant plugin install vagrant-proxyconf
+if ERRORLEVEL 1 GOTO:EOF
+
 cmd /c vagrant up
+if ERRORLEVEL 1 GOTO:EOF
+
 cmd /c vagrant package --base cubievm --output cubievm.box --vagrantfile Vagrantfile.cubievm
+if ERRORLEVEL 1 GOTO:EOF
+
 cmd /c vagrant box add --force cubievm cubievm.box
-cd ..
+if ERRORLEVEL 1 GOTO:EOF
+
+:EOF
---
+++ setupCubieVM.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set +i
+PACKAGELIST=" \
+	git \
+	vim \
+	build-essential \
+	gcc-arm-linux-gnueabihf \
+	qemu-kvm-extras-static \
+	kpartx \
+	"
+
+run_ok()
+{
+	local CMD
+	CMD="$@"
+	local RET
+	echo "executing $CMD" 1>&2
+	eval $CMD
+	RET=$?
+	if [ $RET != 0 ]; then
+		echo "failed with code $RET" 1>&2
+		exit 1
+	fi
+}
+
+run_ok sudo apt-get update
+run_ok sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+
+run_ok sudo apt-get install -y $PACKAGELIST
